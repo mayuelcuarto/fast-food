38c70b35ec2a6dbd68363ba53549e4bf37ba2ef9
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-28 04:50:02 -0500

    Cambios en app_ts
    - Import de BrowserAnimationsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MainMenuComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
