eda1bd1906314e7ed07950149cf1f32ca7300c10
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-28 04:23:32 -0500

    Generación de Componentes y Servicio
    - Con el comando ng g c, se generaron nuevos componentes.
    - Con el comando ng g s, se generó un nuevo servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { OrdersListComponent } from './components/orders/orders-list/orders-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    OrdersComponent,
+    OrdersListComponent
   ],
   imports: [
     BrowserModule,
