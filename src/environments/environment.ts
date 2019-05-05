b132f049b55b8c303ea7633953f13cecc8df612c
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-05 09:06:43 -0500

    Cambios en environment_ts
    - Añadiendo credenciales del proyecto al archivo environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  configFirebase: {
+    apiKey: "AIzaSyAk--Z1c0FdE2vg4OVOtuJsLDSIYUhp_ds",
+    authDomain: "fastfood-552e9.firebaseapp.com",
+    databaseURL: "https://fastfood-552e9.firebaseio.com",
+    projectId: "fastfood-552e9",
+    storageBucket: "fastfood-552e9.appspot.com",
+    messagingSenderId: "573077917080",
+    appId: "1:573077917080:web:f0626fc9167cbdef"
+  }
 };
 
 /*
