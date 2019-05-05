a5dd6eea1054c9cb9e8c0708858819e4577f0de7
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-05 10:48:26 -0500

    Cambios en orders_component
    - Importación del nuevo "OrdersService".
    - Añadiendo import al constructor.
    - Creación de arreglo de pruebas.
    - Creando métodos para añadir y remover items temporales.

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../shared/orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrdersService) { }
+  products = [
+  	{
+  		name: 'Chips',
+  	 	price: 4
+  	},
+  	{
+  		name: 'Humburger',
+  	 	price: 8
+  	},
+  	{
+  		name: 'Chicken',
+  	 	price: 9
+  	},
+  	{
+  		name: 'Fish',
+  	 	price: 6
+  	},
+  	{
+  		name: 'Dessert',
+  	 	price: 3
+  	},
+  	{
+  		name: 'Sauce',
+  	 	price: 1
+  	},
+  	{
+  		name: 'Coffe',
+  	 	price: 2
+  	}
+  ];
+
   appName: string = 'Fast Food!';
+  totalOrder = 0;
+  tempOrder = [];
 
   ngOnInit() {
   }
 
+  onAddProduct(product){
+  	console.log(product);
+  	this.totalOrder = (this.totalOrder + product.price);
+  	this.tempOrder.push(product.name);
+  }
+
+  removeItemTempOrder = (order) => {
+  	let index = this.tempOrder.indexOf(order);
+  	if(index > -1) this.tempOrder.splice(index, 1);
+  }
+
+  onSubmit(){
+  	console.log(this.orderService.myForm.value);
+  }
 }
