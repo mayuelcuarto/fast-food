c52b14a2f739c2cf4e20dbaffe6924bef9a90674
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-05 09:08:04 -0500

    Cambios en service_service
    - Añadiendo importaciones necesarias para trabajar con firebase, observable y map.
    - Creando collección de firebase.
    - Creando métodos necesarios para operar con la dara de firebase.

--- src/app/shared/services.service.ts
+++ src/app/shared/services.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
+	private ordersCollection: AngularFirestoreCollection<any>;
+	orders: Observable<any[]>;
 
-  constructor() { }
+  constructor(private readonly afs: AngularFirestore) {
+  	this.ordersCollection = afs.collection<any>('orders');
+  	this.orders = this.ordersCollection.snapshotChanges().pipe(map(
+  		actions => actions.map(a => {
+  			const data = a.payload.doc.data() as any;
+  			const id = a.payload.doc.id;
+  			return {id, ...data};
+  		})
+  		));
+   }
 
   myForm = new FormGroup ({
   	customerName: new FormControl(''),
@@ -16,18 +30,18 @@ export class ServicesService {
   });
 
   getOrders(){
-
+  	return this.orders;
   }
 
-  updateOrder(){
-
+  updateOrder(order: any){
+  	return this.ordersCollection.doc(order.id).update(order);
   }
 
-  deleteOrder(){
-
+  deleteOrder(id: string){
+  	return this.ordersCollection.doc(id).delete();
   }
 
-  createOrder(){
-  	
+  createOrder(order: any){
+  	return this.ordersCollection.add(order);
   }
 }
