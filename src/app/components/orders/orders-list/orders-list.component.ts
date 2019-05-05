f9bf4157fff61882a6cadd5adfd2eb8c05feecb3
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-05 13:17:41 -0500

    Cambios en orders-list_ts
    - Importación de OrdersService.
    - Eliminación de arreglo plano que fue usado a fines de muestra.
    - Cambiando nombre de columnas de inicialización de dataTable.
    - Añadiendo métodos necesarios para la comunicación con firebase.

--- src/app/components/orders/orders-list/orders-list.component.ts
+++ src/app/components/orders/orders-list/orders-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatSort, MatTableDataSource} from '@angular/material';
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { OrdersService } from '../../../shared/orders.service';
 
 export interface PeriodicElement {
   name: string;
@@ -8,19 +9,6 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
@@ -28,13 +16,34 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class OrdersListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  constructor(private orderService: OrdersService){}
+
+  displayedColumns: string[] = ['orderNumber', 'customerName', 'order', 'completed', 'totalOrder', 'actions'];
+  dataSource = new MatTableDataSource();
 
   @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
+    // Get All Orders
+    this.getAllOrders();
+  }
+
+  ngAfterViewInit(){
     this.dataSource.sort = this.sort;
   }
 
+  getAllOrders(){
+    this.orderService.getOrders().subscribe(res => {
+      this.dataSource.data = res;
+    });
+  }
+
+  onDelete(id: string){
+    this.orderService.deleteOrder(id);
+  }
+
+  onChangeStatus(order: any){
+    order.completed = true;
+    this.orderService.updateOrder(order);
+  }
 }
