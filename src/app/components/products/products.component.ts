32685bdc793e2e424c1ad821da8f1c015b4e43cf
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-05-07 17:40:01 -0500

    Cambios en products_ts
    - Añadiendo imports necesarios para dialogs, además del Inject.
    - Importación de ProductInterface y ProductModalComponent.
    - Creación de métodos necesarios para invocar dialog.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatSort, MatTableDataSource, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ProductsService } from '../../shared/products.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { ProductInterface } from '../../models/product';
+import { ProductModalComponent } from '../../modals/product-modal/product-modal.component';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
 
-  constructor(private productService: ProductsService) { }
-  displayedColumns: string[] = ['name', 'price'];
+  constructor(private productService: ProductsService, public dialog: MatDialog) { }
+  displayedColumns: string[] = ['name', 'price', 'actions'];
   dataSource = new MatTableDataSource();
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ProductModalComponent, {
+      width: '250px',
+      data: {selectedProduct: this.productService.selectedProduct}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+    });
+  }
 
   @ViewChild(MatSort) sort: MatSort;
 
@@ -26,9 +33,29 @@ export class ProductsComponent implements OnInit {
   	this.getAllProducts();
   }
 
+  ngAfterViewInit(){
+    this.dataSource.sort = this.sort;
+  }
+
   getAllProducts(){
     this.productService.getProducts().subscribe(res => {
       this.dataSource.data = res;
     });
+  }
+
+  onChangeStatus(product: ProductInterface){
+    this.productService.updateProduct(product);
+  }
+
+  onPreUpdateProduct(product: ProductInterface){
+    this.productService.selectedProduct = Object.assign({}, product);
+    this.openDialog();
+  }
+
+  onDeleteProduct(idProduct: string){
+    const confirmacion = confirm('Are you sure?');
+    if(confirmacion){
+      this.productService.deleteProduct(idProduct);
+    }
   }
 }
